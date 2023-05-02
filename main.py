1c4b9aba1be4f01085e6ffe8d9c32bab5a53faf8
Hamza Alam <getright_02@live.com>
2023-05-02 22:41:09 +0200

    server

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from graphene import ObjectType, String, Schema
+
+class User(ObjectType):
+    name = String()
+    email = String()
+
+class Query(ObjectType):
+    me = User(name="John Doe", email="john.doe@example.com")
+
+schema = Schema(query=Query)
+
+app = FastAPI()
+
+@app.get('/')
+async def graphql(query: str):
+    result = schema.execute(query)
+    return {'data': result.data}
+
+@app.get('/health')
+async def health():
+    return {'status': 'ok'}
